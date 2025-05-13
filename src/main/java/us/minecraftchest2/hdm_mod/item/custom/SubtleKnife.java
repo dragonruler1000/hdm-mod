a1954505014ad6ae6a945e5989b6daabb6c3730d
dragonruler1000 <github@zach.minec1.us>
2025-05-13 16:58:44 -0500

    Added a window block and working on portal stuff.
    Signed-off-by: dragonruler1000 <github@me.minecraftchest2.us>

--- src/main/java/us/minecraftchest2/hdm_mod/item/custom/SubtleKnife.java
+++ src/main/java/us/minecraftchest2/hdm_mod/item/custom/SubtleKnife.java
@@ -1,16 +1,21 @@
 package us.minecraftchest2.hdm_mod.item.custom;
 
-import jdk.nashorn.internal.ir.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.ScoreTextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 
 public class SubtleKnife extends Item {
@@ -23,19 +28,25 @@ public class SubtleKnife extends Item {
     @Override
     public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
         World world = context.getWorld();
-//        BlockPos pos =;
         PlayerEntity player = context.getPlayer();
 
-        String message = "Item Used";
-        ITextComponent msg = new StringTextComponent(message);
-        assert player != null;
-        player.sendMessage(msg, player.getUniqueID());
-//        world.setBlockState(pos);
-//        if(world.isRemote) {
-//
-//        }
-        return super.onItemUseFirst(stack, context);
+        if (player == null) return ActionResultType.PASS;
+
+        // Get the block being clicked
+        BlockPos blockPos = context.getPos();
+        Direction face = context.getFace(); // The face the player clicked
+
+        // Offset to the block face to place the block on the adjacent block (like right-clicking a wall places on it)
+        BlockPos placePos = blockPos.offset(face);
+
+        // Server-side logic only: place a block
+        if (!world.isRemote) {
+            world.setBlockState(placePos, Blocks.STONE.getDefaultState()); // Change to your portal block later
+        }
+
+        return ActionResultType.SUCCESS;
     }
+
 
 
 }
